42adf8621477e671e4b2044092e39ceacd362a34
abirbenkhaled <abouurabenkhaled@gmail.com>
2021-04-24 04:16:48 +0200

    first commit

--- database/migrations/2021_04_24_020720_create_events_table.php
+++ database/migrations/2021_04_24_020720_create_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClubsTable extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,15 @@ class CreateClubsTable extends Migration
      */
     public function up()
     {
-        Schema::create('clubs', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('recepteur');
+            $table->date('date');
+            $table->string('sujet');
+            $table->integer('club_id')->unsigned();
+            $table->foreign('club_id')->references('id')->on('clubs')->onDelete('restrict')->onUpdate('restrict');
             $table->timestamps();
+
         });
     }
 
@@ -26,6 +32,6 @@ class CreateClubsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clubs');
+        Schema::dropIfExists('events');
     }
 }

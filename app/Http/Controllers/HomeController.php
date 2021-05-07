9ceeafafb48f0e0bb39808ee634ef226b9f09840
abirbenkhaled <abouurabenkhaled@gmail.com>
2021-05-07 23:43:43 +0200

    template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('welcome');
     }
 
     /**
@@ -24,5 +24,14 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    public function welcome(){
+        $events= event::get('id');
+        $resevents= resevent::get('id')
+        return view('welcome', [
+            'events'=>$events,
+            'resevents'=>$resevents
+
+        ] );
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,9 +72,13 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class=" container py-4">
             @yield('content')
         </main>
+          <!-- Footer-->
+          <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
     </div>
 </body>
 </html>

2fd594142dfb64171090bf4c2e541a1b1a1ec3b2
nourhenekhemiri <nourhenekhmiri46@gmail.com>
2021-05-18 23:03:34 +0100

    derniere modification

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,14 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+    public function redirectTo()
+    {
+        if (Auth::user()->admin){
+            return '/admin-dashboard';
+        }
+        return '/home';
+    }
 
     /**
      * Create a new controller instance.
